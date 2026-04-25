6648ac3e1c48a0aec7ec48dab61a4aee086f1588
Ahmad Mohaddeszadeh <ahmad.mohaddeszadeh@gmail.com>
2026-04-25 23:55:41 +0200

    fix: add index to export package

---
+++ packages/export/src/index.ts
@@ -0,0 +1,7 @@
+export { buildDatevHeader } from './datev-header';
+export { mapInvoiceToDatevRow, serialiseDatevRow } from './datev-mapper';
+export type { MapOptions } from './datev-mapper';
+export { generateDatevCsv } from './csv-generator';
+export type { CsvGeneratorOptions, GeneratedCsv } from './csv-generator';
+export { generateZipArchive } from './zip-generator';
+export type { GeneratedZip } from './zip-generator';
